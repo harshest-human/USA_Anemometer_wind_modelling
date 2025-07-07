c30057edb5c38ea9ac97fd1a301797b86e527814
Harsh Sahu <harshsahu.8085@gmail.com>
2025-07-07 16:08:59 +0200

    weekly windroses

--- LVAT_windroses_script.R
+++ LVAT_windroses_script.R
@@ -13,23 +13,34 @@ library(dplyr)
 library(openair)
 library(rvest)
 
-################# Windmast Import ########################
+############ Windmast Import ################
 windmast_input = read.csv("D:/Data Analysis/LVAT_Animal_Temperature_data/2025.04.08-2025.06.30_ATB_5_min_wind_speed_and_direction.csv")
 
 # Convert to datetime
 windmast_input$date <- as.POSIXct(paste(windmast_input$date, windmast_input$time), 
                                   format = "%d.%m.%Y %H:%M:%S")
 
-# Filter measurement period
-windmast_input <- windmast_input %>% 
-        filter(date >= "2025-04-08 12:00:00",
-               date <= "2025-04-15 23:00:00")
+# Filter the date range of interest
+windmast_input <- windmast_input %>%
+        filter(date >= as.POSIXct("2025-04-08 00:00:00"),
+               date <= as.POSIXct("2025-06-30 23:59:59"))
 
-###################### WIND GRAPH #########################
-windRose(windmast_input, 
-         ws = "wind_speed", 
+# Add a 'week' factor column (start of week as label)
+windmast_input <- windmast_input %>%
+        mutate(week = paste0("Week ", isoweek(date), ": ", format(floor_date(date, "week", week_start = 1), "%d.%m"), 
+                             " - ", format(floor_date(date, "week", week_start = 1) + days(6), "%d.%m")))
+
+# Optional: Check how many weeks there are
+unique(windmast_input$week)
+
+
+############ Windrose Visualization ################
+# Create weekly wind roses using openair's `type` argument
+windRose(windmast_input,
+         ws = "wind_speed",
          wd = "wind_direction",
-         breaks = c(0,1,2,4,6,12),
+         type = "week",                # Facet by week
+         breaks = c(0, 1, 2, 4, 6, 12),
          auto.text = FALSE,
          paddle = FALSE,
          grid.line = 5,
@@ -43,6 +54,4 @@ windRose(windmast_input,
          key.position = "bottom",
          par.settings = list(axis.line = list(col = "lightgray")),
          col = c("#4f4f4f", "#0a7cb9", "#f9be00", "#ff7f2f", "#d7153a"))
-
-
 
